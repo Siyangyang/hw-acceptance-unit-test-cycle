29877ae9f65cb17ee50bd4cc1004f371332b7ac5
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2020-10-14 05:34:35 +0000

    more tests

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -5,33 +5,30 @@ RSpec.describe MoviesController, type: :controller do
   describe "MoviesController" do
     render_views
    
-    context "Showing a Movie" do
+    context "how to show a Movie" do
       before :each do
-        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        Movie.create(title: 'test1', rating: 'R', director: 'testdir', release_date: Date.new(2020,1,1))
         @movies = Movie.all
       end
 
       it "Should be show a movie" do
         get :show, id: @movies.take.id
-      
         expect(assigns(:movie)).to eq(@movies.take)
       end
 
     end
 
 
-    context "Creating Movies" do
+    context "how to create Movies" do
       before :each do
-        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        Movie.create(title: 'test1', rating: 'R', director: 'testdir', release_date: Date.new(2020,1,1))
         @movies = Movie.all
       end
 
       it "Should be create a movie" do
-
         movies_count = Movie.all.count
-        movie = {title: 'Star Wars', director: 'George Lucas',rating: 'PG', description: 'Great Movie', release_date: Date.new(1977,5,25)}
+        movie = {title: 'test1', director: 'testdir',rating: 'R', description: 'Movie', release_date: Date.new(2020,1,1)}
         post :create, movie: movie
-      
         expect(flash[:notice]).to eq("#{movie[:title]} was successfully created.")
         expect(response).to redirect_to(movies_path)
         expect(@movies.count).to eq(movies_count + 1)
@@ -39,44 +36,41 @@ RSpec.describe MoviesController, type: :controller do
 
     end
 
-    context "Editing a Movie" do
+    context "how to edit a Movie" do
       before :each do
-        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        Movie.create(title: 'test1',director: 'testdir',rating: 'R', release_date: Date.new(2020,1,1))
         @movies = Movie.all
       end
 
       it "Should be edit a movie" do
         get :edit, id: @movies.take.id
-      
         expect(assigns(:movie)).to eq(@movies.take)
       end
-
     end
 
 
-    context "Updating Movies" do
+    context "how to update Movies" do
       before :each do
-        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        Movie.create(title: 'test1',director: 'testdir',rating: 'R', release_date: Date.new(2020,1,1))
         @movies = Movie.all
       end
 
       it "Should be updating a movie" do
         movie = @movies.take
-        movie_param = {title: 'Star Wars: A new Hope'}
+        movie_param = {title: 'test2'}
         put :update, id: movie.id, movie: movie_param
       
         expect(flash[:notice]).to eq("#{movie_param[:title]} was successfully updated.")
         expect(response).to redirect_to(movie_path(movie.id ))
-        expect(Movie.find(movie.id).title).to eq('Star Wars: A new Hope')
+        expect(Movie.find(movie.id).title).to eq('test2')
 
       end
 
     end
-
-
-    context "Destroy Movies" do
+    
+    context "how to delete Movies" do
       before :each do
-        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        Movie.create(title: 'test1',director: 'testdir',rating: 'R', release_date: Date.new(2020,1,1))
         @movies = Movie.all
       end
 
@@ -95,9 +89,9 @@ RSpec.describe MoviesController, type: :controller do
 
     context "Sorting Movies" do
       before :each do
-        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
-        Movie.create(title: 'Blade Runner', rating: 'PG', director: 'Ridley Scott', release_date: Date.new(1982,6,25))
-        Movie.create(title: 'THX-1138', rating: 'R', director: 'George Lucas', release_date: Date.new(1971,3,11))
+        Movie.create(title: 'test1',director: 'testdir1',rating: 'R', release_date: Date.new(2020,1,1))
+        Movie.create(title: 'test2',director: 'testdir2',rating: 'PG', release_date: Date.new(2020,1,2))
+        Movie.create(title: 'test3',director: 'testdir3',rating: 'PG-13', release_date: Date.new(2020,1,3))
 
         @movies = Movie.all
       end
@@ -115,9 +109,26 @@ RSpec.describe MoviesController, type: :controller do
       end
 
     end
+ context "find similar Movies" do
+      before :each do
+        Movie.create(title: 'test1',director: 'testdir1',rating: 'R', release_date: Date.new(2020,1,1))
+        Movie.create(title: 'test2',director: 'testdir2',rating: 'PG', release_date: Date.new(2020,1,2))
+        Movie.create(title: 'test3',director: 'testdir3',rating: 'PG-13', release_date: Date.new(2020,1,3))
+        @movies = Movie.all
+      end
 
- 
+      it "Should be redirect to the home page with an error when can't find similar movies" do
+        expect(Movie).to receive(:similar_movies).with('test4')
+        
+        get :search,  { title: 'test4' }
+        expect(response).to redirect_to('/movies')
+        expect(flash[:notice]).to eq("'test4' has no director info")
+      end
+
       
+
+    end
+
   end
 
 end
