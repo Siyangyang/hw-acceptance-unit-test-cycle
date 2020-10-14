c98682fc361bce6fc45a08f039d69ef73341de5a
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2020-10-14 05:01:01 +0000

    spec test

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -1,72 +1,123 @@
 require 'rails_helper'
 
-describe MoviesController, type: :controller do
+RSpec.describe MoviesController, type: :controller do
 
-    describe '.similar' do
-        
-        it "found similar movies" do
-            Movie.create(title: 'Movie1', rating: 'G', director: 'Director1', release_date: Date.new(2003,10,10))
-            Movie.create(title: 'Movie2', rating: 'G', director: 'Director1', release_date: Date.new(2004,1,1))
-            Movie.create(title: 'Movie3', rating: 'G', director: 'Director2', release_date: Date.new(2005,5,5))
-            @movies = Movie.all
-    
-            movie = @movies.take
-            get :similar, movie_id: movie.id
-            expect(response).to render_template('movies/similar')
-        end
-        
-        it 'not found movies' do
-            Movie.create(title: 'Movie1', rating: 'G', director: 'Director1', release_date: Date.new(2003,10,10))
-            Movie.create(title: 'Movie2', rating: 'G', director: 'Director1', release_date: Date.new(2004,1,1))
-            Movie.create(title: 'Movie3', rating: 'G', director: 'Director2', release_date: Date.new(2005,5,5))
+  describe "MoviesController" do
+    render_views
+   
+    context "Showing a Movie" do
+      before :each do
+        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        @movies = Movie.all
+      end
+
+      it "Should be show a movie" do
+        get :show, id: @movies.take.id
+      
+        expect(assigns(:movie)).to eq(@movies.take)
+      end
+
+    end
 
 
-            movie = Movie.all.where(title: 'Movie3').take
-            get :similar, movie_id: movie.id
-            expect(response).to redirect_to('/movies')
-        end
-    end
-    
-    describe ".show" do
-        it 'show a movie by id' do
-            Movie.create(title: 'Movie1', rating: 'G', director: 'Director1', release_date: Date.new(2003,10,10))
-            get :show, id: 1
-            expect(response).to render_template('movies/show')
-        end
-    end
-    
-    describe ".create" do
-        it "creates a movie" do
-            movie_p = Hash.new
-            movie_p["title"] = "Movie1"
-            movie_p["rating"] = "G"
-            movie_p["director"] = "Director1"
-            movie_p["release_date"] = Date.new(2003,10,10)
-            
-            post :create, movie: movie_p
-            expect(response).to redirect_to("/movies")
-        end
-    end
-    
-    describe ".update" do
-        it "updates a movie" do
-            Movie.create(title: 'Movie1', rating: 'G', director: 'Director1', release_date: Date.new(2003,10,10))
-            movie_p = Hash.new
-            movie_p["title"] = "Movie1"
-            movie_p["rating"] = "G"
-            movie_p["director"] = "Director1"
-            movie_p["release_date"] = Date.new(2003,10,10)
-            
-            movie = Movie.all.where(id: 1).take
-            get :update, id: 1, movie: movie_p
+    context "Creating Movies" do
+      before :each do
+        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        @movies = Movie.all
+      end
 
-            expect(response).to redirect_to("/movies/#{movie.id}")
-        end
+      it "Should be create a movie" do
+
+        movies_count = Movie.all.count
+        movie = {title: 'Star Wars', director: 'George Lucas',rating: 'PG', description: 'Great Movie', release_date: Date.new(1977,5,25)}
+        post :create, movie: movie
+      
+        expect(flash[:notice]).to eq("#{movie[:title]} was successfully created.")
+        expect(response).to redirect_to(movies_path)
+        expect(@movies.count).to eq(movies_count + 1)
+      end
+
     end
-    
-    describe ".index" do
-        it "sorts by title" do
-            get :index, sort: "title"
-        end
+
+    context "Editing a Movie" do
+      before :each do
+        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        @movies = Movie.all
+      end
+
+      it "Should be edit a movie" do
+        get :edit, id: @movies.take.id
+      
+        expect(assigns(:movie)).to eq(@movies.take)
+      end
+
     end
+
+
+    context "Updating Movies" do
+      before :each do
+        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        @movies = Movie.all
+      end
+
+      it "Should be updating a movie" do
+        movie = @movies.take
+        movie_param = {title: 'Star Wars: A new Hope'}
+        put :update, id: movie.id, movie: movie_param
+      
+        expect(flash[:notice]).to eq("#{movie_param[:title]} was successfully updated.")
+        expect(response).to redirect_to(movie_path(movie.id ))
+        expect(Movie.find(movie.id).title).to eq('Star Wars: A new Hope')
+
+      end
+
+    end
+
+
+    context "Destroy Movies" do
+      before :each do
+        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        @movies = Movie.all
+      end
+
+      it "Should be destroy a movie" do
+        movies_count = Movie.all.count
+        movie = @movies.take
+        delete :destroy, id: movie.id 
+      
+        expect(flash[:notice]).to eq("Movie '#{movie.title}' deleted.")
+        expect(response).to redirect_to(movies_path)
+        expect(@movies.count).to eq(movies_count -1)
+      end
+
+    end
+
+
+    context "Sorting Movies" do
+      before :each do
+        Movie.create(title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: Date.new(1977,5,25))
+        Movie.create(title: 'Blade Runner', rating: 'PG', director: 'Ridley Scott', release_date: Date.new(1982,6,25))
+        Movie.create(title: 'THX-1138', rating: 'R', director: 'George Lucas', release_date: Date.new(1971,3,11))
+
+        @movies = Movie.all
+      end
+
+      it "Should be sort based on title" do
+        get :index, sort: 'title'
+      end
+
+      it "Should be sort based on release_date" do
+        get :index, sort: 'release_date'
+      end
+
+      it "Should be show all movies if sort params is not provided" do
+        get :index
+      end
+
+    end
+
+ 
+      
+  end
+
 end
